28c672ec107b9fe0ced754825ee2de3382eaf821
Your Name <bjorne@gah.se>
2009-04-16 03:25:42 +0200

    using syms instead of strings for config, and some other minor business

--- megatwat.rb
+++ megatwat.rb
@@ -6,17 +6,16 @@ require 'iconv'
 
 @coder = HTMLEntities.new
 
-config = YAML.load_file('config.yml')
-@config = config['config']
+@config = YAML.load_file('config.yml')
 
-unless @config['username'] and @config['password'] and @config['timestamp_file'] and @config['interval']
+unless @config[:username] and @config[:password] and @config[:timestamp_file] and @config[:interval]
   puts "ERROR: username or password or timestamp_file or interval not set in config.yml"
   exit 
 end
 
 # read a timestamp if the timestamp file exists, otherwise set timestamp to past time
 # 2*interval, not too long ago (to stop it from answering very old replies)
-@last_timestamp = File.exists?(@config['timestamp_file']) ? Time.parse(File.new(@config['timestamp_file'], 'r').gets) : Time.now - 2*@config['interval']
+@last_timestamp = File.exists?(@config[:timestamp_file]) ? Time.parse(File.new(@config[:timestamp_file], 'r').gets) : Time.now - 2*@config[:interval]
 
 puts ">> Last timestamp: #{@last_timestamp}"
 puts ">> Bot loaded."
@@ -32,14 +31,14 @@ puts ">> MegaHAL possibly loaded."
 
 while true
   
-  if @last_timestamp > Time.now - @config['interval']
-    puts ">> Sleeping #{@config['interval']} seconds."
-    sleep @config['interval']
+  if @last_timestamp > Time.now - @config[:interval]
+    puts ">> Sleeping #{@config[:interval]} seconds."
+    sleep @config[:interval]
   end
 
   puts ">> Checking new replies."
 
-  httpauth = Twitter::HTTPAuth.new(@config['username'], @config['password'])
+  httpauth = Twitter::HTTPAuth.new(@config[:username], @config[:password])
   base = Twitter::Base.new(httpauth)
 
   base.replies.each do |tweet| 
@@ -48,7 +47,7 @@ while true
     puts ">> Answering reply by #{tweet.user.screen_name}: #{tweet.text}."
     
     # tell megahal about the text
-    io.write(tweet.text.gsub(/\@#{@config['username']}/, '') + "\n\n") # needs double newlines!
+    io.write(tweet.text.gsub(/\@#{@config[:username]}/, '') + "\n\n") # needs double newlines!
     # get response and reply with it
     reply = io.gets
     File.open('last_reply.txt', 'w') {|f| f.write(reply) }
@@ -58,7 +57,7 @@ while true
   end
   
   @last_timestamp = Time.now
-  File.open(@config['timestamp_file'], 'w') {|f| f.write(@last_timestamp) }
+  File.open(@config[:timestamp_file], 'w') {|f| f.write(@last_timestamp) }
   
 end
 #base.update('Updating from ruby, yay!')
